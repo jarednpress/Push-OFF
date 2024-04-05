f3b73bae94ffe99b25f3a20838d1da847bb61379
USPrograms <uri.soltz@colorado.edu>
2024-04-05 13:39:11 -0600

    starting player

---
+++ core/src/csci/pushoff/characters/player.java
@@ -0,0 +1,11 @@
+package csci.pushoff.characters;
+
+public class player {
+    private String name = "";
+    private int friction = 100;
+    private int knockback = 10;
+
+    private int sheildStrength = 20;
+
+
+}
