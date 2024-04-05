d3b6ec9efc2595d2950be2ac54545c5871a1bfe7
USPrograms <uri.soltz@colorado.edu>
2024-04-05 15:12:43 -0600

    More player stuff

---
+++ core/src/csci/pushoff/characters/genericPlayer.java
@@ -0,0 +1,7 @@
+package csci.pushoff.characters;
+
+public class genericPlayer extends player{
+    public genericPlayer(){
+        this.setPlayerName("Player1");
+    }
+}
--- core/src/csci/pushoff/characters/player.java
+++ core/src/csci/pushoff/characters/player.java
@@ -1,11 +1,38 @@
 package csci.pushoff.characters;
-
+//import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.BitmapFont;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.Input;
+//import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+//import com.badlogic.gdx.math.Rectangle;
+//import com.badlogic.gdx.utils.ScreenUtils;
+//import csci.pushoff.GdxGameMain;
+//import csci.pushoff.screens.stages.*;
 public class player {
-    private String name = "";
+    private String playerName = "";
+    private  String characterName = "";
     private int friction = 100;
     private int knockback = 10;
 
     private int sheildStrength = 20;
+    private  Texture texture;
+
+//    public player(String name, int friction, int knockback, ){
+//        this.name
+//    }
+    public void  setPlayerName(String name){this.playerName = name;}
+    public String getPlayerName(){return this.playerName;}
+    public void  setCharacterName(String name){this.characterName = name;}
+    public String getCharacterName(){return this.characterName;}
+    public void setFriction(int friction){this.friction = friction;}
+    public int getFriction(){return this.friction;}
+    public void setKnockback(int knockback){this.knockback = knockback;}
+    public int getKnockback(){return this.knockback;}
+
+    public void setTexture(Texture texture){this.texture = texture;}
+    public Texture getTexture(){return this.texture;}
 
 
 }
